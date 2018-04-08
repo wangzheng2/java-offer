248c0e7d37debcac6a95f83b84a94f8ef4f49a04
Administrator <Administrator@55S9W73KBCLBN09>
2018-04-08 20:57:55 +0800

    test

--- src/com/wz/solution/Array/searchTwoDimensionalArray.java
+++ src/com/wz/solution/Array/searchTwoDimensionalArray.java
@@ -1,7 +1,7 @@
 //问题：在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
 //      请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 //思路:从右上角开始，如果比它大就向右查找，比它小就向左查找。
-package com.wz.sulution.Array;
+package com.wz.solution.Array;
 public class searchTwoDimensionalArray {
 	public boolean Find(int target,int[][] array){
 		   int hang=array.length;
